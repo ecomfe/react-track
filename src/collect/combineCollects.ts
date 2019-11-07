f60dd471e0ff34d6c883f7802829800d02bd9198
chenze03 <chenze03@baidu.com>
2019-11-07 21:33:48 +0800

    feat: upgrade reskript to emit typescript declaration

--- src/collect/combineCollects.ts
+++ src/collect/combineCollects.ts
@@ -1,19 +1,22 @@
 import {CollectGenerator} from '../types';
 
-function combineCollect<T1, T2>(collect1: CollectGenerator<T1>, collect2: CollectGenerator<T2>): (...args) => T1 & T2;
+function combineCollect<T1, T2>(
+    collect1: CollectGenerator<T1>,
+    collect2: CollectGenerator<T2>
+): (...args: any[]) => T1 & T2;
 
 function combineCollect<T1, T2, T3>(
     collect1: CollectGenerator<T1>,
     collect2: CollectGenerator<T2>,
     collect3: CollectGenerator<T3>
-): (...args) => T1 & T2 & T3;
+): (...args: any[]) => T1 & T2 & T3;
 
 function combineCollect<T1, T2, T3, T4>(
     collect1: CollectGenerator<T1>,
     collect2: CollectGenerator<T2>,
     collect3: CollectGenerator<T3>,
     collect4: CollectGenerator<T4>
-): (...args) => T1 & T2 & T3 & T4;
+): (...args: any[]) => T1 & T2 & T3 & T4;
 
 function combineCollect<T1, T2, T3, T4, T5>(
     collect1: CollectGenerator<T1>,
@@ -21,7 +24,7 @@ function combineCollect<T1, T2, T3, T4, T5>(
     collect3: CollectGenerator<T3>,
     collect4: CollectGenerator<T4>,
     collect5: CollectGenerator<T5>
-): (...args) => T1 & T2 & T3 & T4 & T5;
+): (...args: any[]) => T1 & T2 & T3 & T4 & T5;
 
 function combineCollect<T1, T2, T3, T4, T5, T6>(
     collect1: CollectGenerator<T1>,
@@ -30,7 +33,7 @@ function combineCollect<T1, T2, T3, T4, T5, T6>(
     collect4: CollectGenerator<T4>,
     collect5: CollectGenerator<T5>,
     collect6: CollectGenerator<T6>
-): (...args) => T1 & T2 & T3 & T4 & T6;
+): (...args: any[]) => T1 & T2 & T3 & T4 & T6;
 
 function combineCollect(...collects: Array<CollectGenerator<unknown>>) {
     return (...args: any[]) =>
