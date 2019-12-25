cf728f559cb28c1aa987bdd8ecd58b74d650c3d3
otakustay <otakustay@gmail.com>
2019-12-25 16:57:04 +0800

    auto wrap children in suspense

--- src/components/TrackRoute.tsx
+++ src/components/TrackRoute.tsx
@@ -1,15 +1,37 @@
-import {SFC} from 'react';
+import {Suspense, SFC, ComponentType} from 'react';
 import {Route, RouteProps} from 'react-router-dom';
 import TrackPageView from './TrackPageView';
 
-const TrackRoute: SFC<RouteProps> = ({render, children, component, ...props}) => (
-    <Route {...props}>
-        <TrackPageView>
-            <Route render={render} component={component}>
-                {children}
-            </Route>
-        </TrackPageView>
-    </Route>
-);
+const lazyComponentMapping = new WeakMap<ComponentType, ComponentType>();
+
+const mapToLazyComponent = (ComponentIn: ComponentType) => {
+    if (!lazyComponentMapping.has(ComponentIn)) {
+        const ComponentOut: SFC = props => (
+            <Suspense fallback={null}>
+                <ComponentIn {...props} />
+            </Suspense>
+        );
+        ComponentOut.displayName = `withSuspense(${ComponentIn.displayName || ComponentIn.name || 'Unknown'})`;
+        lazyComponentMapping.set(ComponentIn, ComponentOut);
+    }
+    return lazyComponentMapping.get(ComponentIn);
+};
+
+interface Props extends RouteProps {
+    wrapSuspense?: boolean;
+}
+
+const TrackRoute: SFC<Props> = ({render, children, component, wrapSuspense = false, ...props}) => {
+    const componentPropValue = (wrapSuspense && component) ? mapToLazyComponent(component as ComponentType) : component;
+    return (
+        <Route {...props}>
+            <TrackPageView>
+                <Route render={render} component={componentPropValue}>
+                    {children}
+                </Route>
+            </TrackPageView>
+        </Route>
+    );
+};
 
 export default TrackRoute;
