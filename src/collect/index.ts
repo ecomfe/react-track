6802b0a09fbdc35caf81a146ffa9add6a3095857
chenzhouji <chenzhouji@baidu.com>
2020-06-03 17:11:24 +0800

    feat: add a collect `basename` #27

--- src/collect/index.ts
+++ src/collect/index.ts
@@ -1,3 +1,4 @@
+export {default as basename} from './basename';
 export {default as browser} from './browser';
 export {default as session} from './session';
 export {default as combineCollects} from './combineCollects';
